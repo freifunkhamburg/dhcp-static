396455fc1a458cc9be0e020e43cfd7e85dcf92e5
Jan Winkelmann <jan@unstable.nl>
2013-03-24 16:12:19 +0100

    updated cron-script

--- updateStatics.sh
+++ updateStatics.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+# Simple script to update the dhcp-static files from git
 
-cp static.conf /etc/dhcp/
+# CONFIGURE THIS TO YOUR GIT DIRECTORY
+GIT_REPO=/home/dhcp/dhcp-static
+
+# CONFIGURE THIS TO THE USER TO RUN THE GIT COMMANDS AS
+USER=dhcp
+
+cd $GIT_REPO
+
+su -c "git pull" $USER
+
+cp $GIT_REPO/static.conf /etc/dhcp/static.conf
